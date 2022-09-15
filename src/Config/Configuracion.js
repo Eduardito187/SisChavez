b564904364bc0f7099675784e54c92880f43fbdc
Eduard <eduard@localhost.localdomain>
2022-09-15 00:22:30 -0400

    changes

--- src/Config/Configuracion.js
+++ src/Config/Configuracion.js
@@ -3,3 +3,4 @@ export let URL_APICHAVEZ = "http://localhost/APIChavez/";
 export let URL_LOCAL = "http://localhost:8080";
 export let URL_MI_IP = "http://localhost:8080";
 export let Gestion = 22;
+
